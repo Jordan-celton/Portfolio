4bc1bfeb8ca978e4b7954bc1ffa42d51b20784cc
Dijoyce <celton.jordan@outlook.fr>
2024-10-21 15:36:07 +0200

    add color on pages link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,49 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <nav className="navbar">
       <ul className="navbar-list">
         <li className="navbar-item">
-          <Link to="/about" className="navbar-link">
+          <Link
+            to="/about"
+            className={`navbar-link ${
+              location.pathname === "/about" ? "active" : ""
+            }`}
+          >
             A propos
           </Link>
         </li>
-
         <li className="navbar-item">
-          <Link to="/resume" className="navbar-link">
+          <Link
+            to="/resume"
+            className={`navbar-link ${
+              location.pathname === "/resume" ? "active" : ""
+            }`}
+          >
             Resume
           </Link>
         </li>
-
         <li className="navbar-item">
-          <Link to="/portfolio" className="navbar-link">
+          <Link
+            to="/portfolio"
+            className={`navbar-link ${
+              location.pathname === "/portfolio" ? "active" : ""
+            }`}
+          >
             Portfolio
           </Link>
         </li>
-
         <li className="navbar-item">
-          <Link to="/contact" className="navbar-link">
+          <Link
+            to="/contact"
+            className={`navbar-link ${
+              location.pathname === "/contact" ? "active" : ""
+            }`}
+          >
             Contact
           </Link>
         </li>
