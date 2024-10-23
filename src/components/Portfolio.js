0a591d6f71e6005882cdc67ac23988d4b9b8f7fb
Dijoyce <celton.jordan@outlook.fr>
2024-10-23 14:02:25 +0200

    add traduction on portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 
 // Importation des images
 import bookiImg from "../images/projet-booki.png";
@@ -13,67 +13,63 @@ import qwentaImg from "../images/projet-qwenta.png";
 // Importation de Font Awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { TranslationContext } from "../App"; // Importer le contexte
 
 const Portfolio = () => {
+  const { translations } = useContext(TranslationContext); // Utiliser le contexte
   const [activeCategory, setActiveCategory] = useState("All");
   const [modalData, setModalData] = useState({ title: "", description: "" });
 
   const projects = [
     {
-      title: "Booki",
+      title: translations.bookiTitle || "Booki",
       img: bookiImg,
-      category: "Web development",
-      description:
-        "Création de la page d'accueil d'une agence de voyage avec une interface responsive utilisant HTML5 et CSS3. Intégration de maquettes Figma et développement de composants personnalisés pour une expérience utilisateur optimale.\nLien vers le projet : https://github.com/Jordan-celton/P3",
+      category: translations.webDevelopment || "Web development",
+      description: translations.bookiDescription,
     },
     {
-      title: "Ohmyfood",
+      title: translations.ohmyfoodTitle || "Ohmyfood",
       img: ohmyfoodImg,
-      category: "Web design",
-      description:
-        "Objectifs d'apprentissage : conception mobile-first, implémentation d'animations CSS, utilisation de Sass pour un code maintenable, et versionnement avec Git et GitHub.\nLien vers le projet : https://github.com/Jordan-celton/P4",
+      category: translations.webDesign || "Web design",
+      description: translations.ohmyfoodDescription,
     },
     {
-      title: "Portfolio Architecte Intérieur",
+      title: translations.sophiebluelTitle || "Portfolio Architecte Intérieur",
       img: sophiebluelImg,
-      category: "Web design",
-      description:
-        "Développement d'une page web dynamique pour un site d'architecte d'intérieur avec JavaScript. Interaction avec une API, création de pages dynamiques et utilisation d'outils comme Figma et Visual Studio Code.\nLien vers le projet : https://github.com/Jordan-celton/P6",
+      category: translations.webDesign || "Web design",
+      description: translations.sophiebluelDescription,
     },
     {
-      title: "Refonte du site Kasa",
+      title: translations.kasaTitle || "Refonte du site Kasa",
       img: kasaImg,
-      category: "Applications",
-      description:
-        "Implémentation du front-end d'une application avec React et React Router. Développement de composants, gestion de la navigation et utilisation de données simulées à partir d'un fichier JSON.\nLien vers le projet : https://github.com/Jordan-celton/P7",
+      category: translations.applications || "Applications",
+      description: translations.kasaDescription,
     },
     {
-      title: "Nina Carducci - Optimisation SEO",
+      title:
+        translations.ninaCarducciTitle || "Nina Carducci - Optimisation SEO",
       img: ninaCarducciImg,
-      category: "Web development",
-      description:
-        "Optimisation du référencement d'un site web en améliorant sa performance et son accessibilité. Analyse des problèmes, mise en œuvre d'améliorations et création d'un rapport détaillant les résultats avant/après.\nLien vers le projet : https://github.com/Jordan-celton/P8",
+      category: translations.webDevelopment || "Web development",
+      description: translations.ninaCarducciDescription,
     },
     {
-      title: "724events - Debug et test",
+      title: translations.eventsTitle || "724events - Debug et test",
       img: eventsImg,
-      category: "Web development",
-      description:
-        "Débuggage et finalisation d'un site one-page. Analyse du code, résolution de bugs avec les Chrome DevTools et React Developer Tools, documentation des tests, et gestion de projet avec Yarn.\nLien vers le projet : https://github.com/Jordan-celton/P9",
+      category: translations.webDevelopment || "Web development",
+      description: translations.eventsDescription,
     },
     {
-      title: "Argent Bank - Dashboard utilisateur",
+      title:
+        translations.argentBankTitle || "Argent Bank - Dashboard utilisateur",
       img: argentBankImg,
-      category: "Applications",
-      description:
-        "Développement du front-end d'une application bancaire avec React et Redux. Intégration des appels API, création d'une interface utilisateur responsive et gestion de l'état global avec Redux.\nLien vers le projet : https://github.com/Jordan-celton/P10",
+      category: translations.applications || "Applications",
+      description: translations.argentBankDescription,
     },
     {
-      title: "Menu Maker - Qwenta",
+      title: translations.qwentaTitle || "Menu Maker - Qwenta",
       img: qwentaImg,
-      category: "Web development",
-      description:
-        "Développement du projet **Menu Maker - Qwenta**, un outil en ligne pour la création et la personnalisation de menus par les restaurateurs.\nLien vers le projet : https://github.com/Jordan-celton/P11",
+      category: translations.webDevelopment || "Web development",
+      description: translations.qwentaDescription,
     },
   ];
 
@@ -113,30 +109,35 @@ const Portfolio = () => {
   return (
     <article className="portfolio" data-page="portfolio">
       <header>
-        <h2 className="h2 article-title">Portfolio</h2>
+        <h2 className="h2 article-title">
+          {translations.portfolioTitle || "Portfolio"}
+        </h2>
       </header>
 
       <section className="projects">
         <ul className="filter-list">
-          {["All", "Web design", "Applications", "Web development"].map(
-            (category) => (
-              <li className="filter-item" key={category}>
-                <button
-                  className={activeCategory === category ? "active" : ""}
-                  onClick={() => setActiveCategory(category)}
-                  data-filter-btn
-                >
-                  {category}
-                </button>
-              </li>
-            )
-          )}
+          {[
+            "All",
+            translations.webDesign || "Web design",
+            translations.applications || "Applications",
+            translations.webDevelopment || "Web development",
+          ].map((category) => (
+            <li className="filter-item" key={category}>
+              <button
+                className={activeCategory === category ? "active" : ""}
+                onClick={() => setActiveCategory(category)}
+                data-filter-btn
+              >
+                {category}
+              </button>
+            </li>
+          ))}
         </ul>
 
         <div className="filter-select-box">
           <button className="filter-select" data-select>
             <div className="select-value" data-selecct-value>
-              Choisir une catégorie
+              {translations.chooseCategory || "Choisir une catégorie"}
             </div>
             <div className="select-icon">
               <ion-icon name="chevron-down"></ion-icon>
@@ -144,18 +145,21 @@ const Portfolio = () => {
           </button>
 
           <ul className="select-list">
-            {["All", "Web design", "Applications", "Web development"].map(
-              (category) => (
-                <li className="select-item" key={category}>
-                  <button
-                    onClick={() => setActiveCategory(category)}
-                    data-select-item
-                  >
-                    {category}
-                  </button>
-                </li>
-              )
-            )}
+            {[
+              "All",
+              translations.webDesign || "Web design",
+              translations.applications || "Applications",
+              translations.webDevelopment || "Web development",
+            ].map((category) => (
+              <li className="select-item" key={category}>
+                <button
+                  onClick={() => setActiveCategory(category)}
+                  data-select-item
+                >
+                  {category}
+                </button>
+              </li>
+            ))}
           </ul>
         </div>
 
@@ -168,9 +172,8 @@ const Portfolio = () => {
               data-category={project.category}
             >
               <button
-                href="#"
                 onClick={(e) => {
-                  e.preventDefault(); // Empêche le comportement par défaut du lien
+                  e.preventDefault();
                   openModal(project.title, project.description);
                 }}
               >
